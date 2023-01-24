748b3cb4dccd0f1c75f44b88cb9089f43c549fbf
sayed6201 <sayed.ahmed620@gmail.com>
2023-01-24 12:21:31 +0000

    travel ui created

---
+++ App.js
@@ -0,0 +1,8 @@
+import TourDetailScreen from "./app/screens/TourDetailScreen";
+import TOURS from "./app/config/TOURS";
+import HomeScreen from "./app/screens/HomeScreen";
+
+export default function App() {
+  // return <TourDetailScreen tour={TOURS[3]} />;
+  return <HomeScreen />;
+}
